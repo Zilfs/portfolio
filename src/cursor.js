80d3aa59a5ce8a321f764bfc152472e97689e34b
Zilfs <rheinlandcast@gmail.com>
2024-11-22 23:50:48 +0700

    fix: overflow animated cursor

--- src/cursor.js
+++ src/cursor.js
@@ -1,5 +1,9 @@
-var cursor = document.querySelector(".cursor");
-
-document.addEventListener("mousemove", e => {
-    cursor.setAttribute("style", "top: " + (e.pageY - 280) + "px; left: " + (e.pageX - 280) + "px;")
+$(document).ready(function () {
+    var cursor = document.querySelector(".cursor");
+    $(document).on("mousemove", function (event) {
+        cursor.setAttribute("style", "top: " + (event.pageY - $(".cursor-space").offset().top - 280) + "px; left: " + (event.pageX - 280) + "px;");
+    });
 });
+
+
+
